8f39df86795b19bc2f36ce79c9e62e321c8ea4cf
Nick Gamberini <nickelpro@gmail.com>
2015-02-06 19:06:09 -0500

    Fix offline demo

--- examples/offlinedemo.py
+++ examples/offlinedemo.py
@@ -6,13 +6,17 @@ from spock import Client
 from spock.plugins import DefaultPlugins
 from demoplugin import DemoPlugin
 
-start_settings = {
-    'username': 'a_username',
-    'authenticated': False,
+settings = {
+    'start': {
+        'username': 'Bot',
+    },
+    'auth': {
+        'authenticated': False,
+    },
 }
 
 plugins = DefaultPlugins
 plugins.append(('demo', DemoPlugin))
-client = Client(plugins = plugins, start = start_settings)
+client = Client(plugins = plugins, settings = settings)
 #client.start() with no arguments will automatically connect to localhost
 client.start('localhost', 25565)
