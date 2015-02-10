82fee1c16f23ff7078e6978b304149519f80416c
Morgan Creekmore <gamingrobot@gmail.com>
2015-02-09 23:14:34 -0700

    Updated antiafk

--- plugins/AntiAFK.py
+++ plugins/AntiAFK.py
@@ -1,28 +1,17 @@
 """
-Hilariously out of date, I'll update this when it's not 3:30 in the morning
-In the meantime, go look at plugins in spock.net.plugins for more up-to-date plugin examples
+Just send the animation packet every 5min
 """
-__author__ = "Nick Gamberini"
+__author__ = "Nick Gamberini, Morgan Creekmroe"
 __copyright__ = "Copyright 2015, The SpockBot Project"
 __license__ = "MIT"
 
-from spock.mcp.mcpacket import Packet
+AFK_TIME = 5
 
-#Very bad and naive Anti-AFK plugin
 class AntiAFKPlugin:
-	def __init__(self, client, settings):
-		self.client = client
-		client.register_dispatch(self.avoid_afk, 0x03)
-		client.register_dispatch(self.revive, 0x08)
+	def __init__(self, ploader, settings):
+		self.net = ploader.requires('Net')
+		self.timers = ploader.requires('Timers')
+		self.timers.reg_event_timer(AFK_TIME, self.avoid_afk)
 
-	def avoid_afk(self, packet):
-		msg = packet.data['text'].lower()
-		if ('afk plugin' in msg):
-			self.client.push(Packet(ident=0x03, data={
-				"text": "Hello, I am Spock_Bot, this message is to avoid AFKGC"
-				})
-			)
-
-	def revive(self, packet):
-		if self.client.health['health']<=0:
-			self.client.push(Packet(ident=0xCD, data={'payload': 1}))
+	def avoid_afk(self):
+		self.net.push_packet('PLAY>Animation', '')
