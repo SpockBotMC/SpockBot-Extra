2f24b0e696d3e8cfa8a3c2b1b045cb86c06a3d55
Lucas BeeSea <Bc1151@Bc1151.com>
2015-11-17 12:19:09 -0500

    Added tpa and tpaccept
    I have been coding a large number of commands for my vanilla server using this project, so I wanted to show my appreciation by offering a couple of them :)

--- plugins/base_commands.py
+++ plugins/base_commands.py
@@ -8,8 +8,12 @@ __license__ = "MIT"
 
 import datetime
 
+import logging
 from spockbot.plugins.base import PluginBase
 from spockbot.vector import Vector3
+import json
+tpareqs = {}
+logger = logging.getLogger('spockbot')
 
 
 class BaseCommandsPlugin(PluginBase):
@@ -23,11 +27,34 @@ class BaseCommandsPlugin(PluginBase):
         'cmd_place': 'handle_place',
         'cmd_break': 'handle_break',
         'cmd_click': 'handle_click',
+        'cmd_tpa': 'handle_tpa',
+        'cmd_tpaccept': 'handle_tpaccept',
     }
 
     def __init__(self, ploader, settings):
         super(BaseCommandsPlugin, self).__init__(ploader, settings)
-
+    def handle_tpa(self, event, data):
+        try:
+            args = data['args']
+            tpareqs[args[0]] = data['name']
+            self.net.push_packet('PLAY>Chat Message', {'message': '/tell ' + ''.join(args[0]) + ' would like to tpa to you, type (!)tpaccept or (!)tpdeny'})
+        except IndexError:
+            self.net.push_packet('PLAY>Chat Message', {'message': '/tell ' + ''.join(data['name']) + ' Usage: (!)tpa [name]'})
+    def handle_tpaccept(self, event, data):
+        #args = data['args']
+        try:
+            towho = data['name']
+            tpareqs[towho]
+            fromwho = tpareqs[towho]
+            if fromwho != "":
+                self.net.push_packet('PLAY>Chat Message', {'message': '/tp ' + fromwho + ' ' + towho})
+                logger.debug(json.dumps(tpareqs))
+                del tpareqs[towho] #clear the table.
+            else:
+                del tpareqs[towho] #clear the table.
+                self.net.push_packet('PLAY>Chat Message', {'message': '/tell ' + ''.join(data['name']) + ' A unknown error occured, try again later.'}) #they don't need to know what happened.
+        except:
+            self.net.push_packet('PLAY>Chat Message', {'message': '/tell ' + towho + ' you have no pending tpa requests.'})
     def handle_jump(self, event, data):
         self.physics.jump()
 
